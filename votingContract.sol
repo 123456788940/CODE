65061d817265ccf4d97897706f0a9a6502d6fcc7
Soham Dutta <123827172+123456788940@users.noreply.github.com>
2024-04-08 08:53:32 +0530

    Update votingContract.sol

--- votingContract.sol
+++ votingContract.sol
@@ -1,1 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12 <0.9.0;
 
+contract vote {
+    address public owner;
+    address[] public members;
+    mapping(address=>bool) public isPaid;
+        mapping(address=>bool) public hasVoted;
+
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function register(address[] memory _address, uint amount) public payable{
+        require(_address.length > 0);
+        require(amount>0);
+        require(!isPaid[msg.sender]);
+        for (uint i = 0; i < _address.length; i++) {
+            members.push(_address[i]);
+
+        }
+         isPaid[msg.sender] = true;
+        payable(msg.sender).transfer(amount);
+    }
+
+    function _vote(address _address) public {
+         require(_address != address(0));
+           require(isPaid[msg.sender]);
+            require(!hasVoted[_address]);
+            hasVoted[_address] = true;
+
+    }
+
+
+    function trackVote(address _address) public view returns(bool) {
+        return hasVoted[_address];
+    }
+}
